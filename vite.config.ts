658a096909be70a66488b51f0344e277a1e84d83
Karbust <charparodar@gmail.com>
2021-11-06 17:10:57 +0000

    First code commit
    Installed React, MUI v5, Emotion, ESLint, TypeScript and Vite.

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { minifyHtml } from 'vite-plugin-html'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  publicDir: 'public',
+  build: {
+    outDir: 'build',
+    terserOptions: { format: { comments: false } },
+  },
+  plugins: [
+    reactRefresh(),
+    minifyHtml()
+  ],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^@mui\/icons-material\/(.*)/,
+        replacement: '@mui/icons-material/esm/$1',
+      },
+    ],
+  },
+})
